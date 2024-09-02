07315e4ff93de349d1557f640c080c31b3c0ca76
Linn <136191464+Linn0001@users.noreply.github.com>
2024-09-01 22:05:16 -0500

    Update database.js

--- database.js
+++ database.js
@@ -4,9 +4,16 @@ const db = new sqlite3.Database('./database.db', (err) => {
   if (err) {
     console.error(err.message);
   } else {
-    console.log('Connected to the SQLite database.');
+    console.log('Conectado a la base de datos SQLite.');
+    // Crear tabla si no existe
+    db.run(`CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        firstname TEXT NOT NULL,
+        lastname TEXT NOT NULL,
+        gender TEXT NOT NULL,
+        age INTEGER NOT NULL
+    )`);
   }
 });
 
 module.exports = db;
-
